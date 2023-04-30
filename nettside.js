3103c0b90873df6febc94f79041e2189fd01c72c
Benjamin-A <benjamin.roald.andersen@gmail.com>
2023-04-30 15:46:16 +0200

    websocket updates websites now

--- nettside.js
+++ nettside.js
@@ -75,14 +75,107 @@ window.addEventListener("DOMContentLoaded", () => {
     websocket.onmessage = ({ data }) => {
         const statuses_obj = JSON.parse(data);
         
-        for (let statuses_key in statuses_obj) {
-            document.getElementById(statuses_key).innerHTML = statuses_obj[statuses_key];
-            
-            if (statuses_obj[statuses_key] == "0") {
-                document.getElementById(statuses_key).style.backgroundColor = "red";
-            } else {
-                document.getElementById(statuses_key).style.backgroundColor = "green";
+        let groupedData = {};
+
+        for (let key in statuses_obj) {
+        let prefix = key.split("_")[0];
+        if (!groupedData[prefix]) {
+        groupedData[prefix] = [];
+        }
+        groupedData[prefix].push(key);
+        }
+        for (let group_key in groupedData){
+            if (group_key == "mens"){
+                for (let i = 0; i < groupedData[group_key].length; i++) {
+                    //update div 
+                    let current_key = groupedData[group_key][i]
+                    let custom_inner_HTML = '<img class="img-fluid icon" src="figures/Toilets_male.svg" style="height: 80%; padding: 0; margin: 0;" alt="Mens Toilet"><p style="padding: 1%; margin: 1%;">'
+                    document.getElementById(current_key).innerHTML = custom_inner_HTML + statuses_obj[current_key] + "</p>";
+                    if (statuses_obj[current_key] == "0") {
+                        document.getElementById(current_key).style.backgroundColor = "red";
+                    }
+                    else if (statuses_obj[current_key] == "1") {
+                        document.getElementById(current_key).style.backgroundColor = "orange";
+                    }
+                    else {
+                        document.getElementById(current_key).style.backgroundColor = "green";
+                    }
+                    
+                    
+                    // alert("  " + groupedData[group_key][i]);
+                  }
+                
             }
+            else if (group_key == "ladies"){
+                for (let i = 0; i < groupedData[group_key].length; i++) {
+                    //update div 
+                    let current_key = groupedData[group_key][i]
+                    let custom_inner_HTML = '<img class="img-fluid icon" src="figures/Toilets_female.svg" style="height: 80%; padding: 0; margin: 0;" alt="Ladies Toilet"><p style="padding: 1%; margin: 1%;">'
+                    document.getElementById(current_key).innerHTML = custom_inner_HTML + statuses_obj[current_key] + "</p>";
+                    if (statuses_obj[current_key] == "0") {
+                        document.getElementById(current_key).style.backgroundColor = "red";
+                    }
+                    else if (statuses_obj[current_key] == "1") {
+                        document.getElementById(current_key).style.backgroundColor = "orange";
+                    }
+                    else {
+                        document.getElementById(current_key).style.backgroundColor = "green";
+                    }
+                    
+                    
+                    // alert("  " + groupedData[group_key][i]);
+                  }
+                
+            }
+            else if (group_key == "handicap"){
+                for (let i = 0; i < groupedData[group_key].length; i++) {
+                    //update div 
+                    let current_key = groupedData[group_key][i]
+                    let custom_inner_HTML = '<img class="img-fluid icon" src="figures/Toilets_handicap.svg" style="height: 80%; padding: 0; margin: 0;" alt="Handicap Toilet"><p style="padding: 1%; margin: 1%;">'
+                    document.getElementById(current_key).innerHTML = custom_inner_HTML + statuses_obj[current_key] + "</p>";
+                    if (statuses_obj[current_key] == "0") {
+                        document.getElementById(current_key).style.backgroundColor = "red";
+                    }
+                    else if (statuses_obj[current_key] == "1") {
+                        document.getElementById(current_key).style.backgroundColor = "orange";
+                    }
+                    else {
+                        document.getElementById(current_key).style.backgroundColor = "green";
+                    }
+                    
+                    
+                    // alert("  " + groupedData[group_key][i]);
+                  }
+                
+            }
+            else if (group_key == "bar"){
+                for (let i = 0; i < groupedData[group_key].length; i++) {
+                    //update div 
+                    let current_key = groupedData[group_key][i]
+                    let custom_inner_HTML = '<img class="img-fluid icon" src="figures/Bar.svg" style="height: 80%; padding: 0; margin: 0;" alt="Bar"><p  style="padding: 1%; margin: 1%;">'
+                    document.getElementById(current_key).innerHTML = custom_inner_HTML + statuses_obj[current_key] + "</p>";
+                    if (statuses_obj[current_key] == "0") {
+                        document.getElementById(current_key).style.backgroundColor = "red";
+                    }
+                    else if (statuses_obj[current_key] == "1") {
+                        document.getElementById(current_key).style.backgroundColor = "orange";
+                    }
+                    else {
+                        document.getElementById(current_key).style.backgroundColor = "green";
+                    }
+                    
+                    
+                    // alert("  " + groupedData[group_key][i]);
+                  }
+                
+            }
+        }
+
+        console.log(groupedData);
+
+        for (let statuses_key in statuses_obj) {
+            
+            
         }  
     };
 });
